badc623f5cc82e5f0bf2bdd6f3e0c3c34c125e6d
deepum <deepumpoudel@gmail.com>
2021-01-23 21:58:47 +0545

    naved it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-<h1>turi vunte</h1>
+    <div>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import React, { useRef } from "react";
+import "./Navbar.css";
+import logo from "../../Images/logo.png";
+
+function Navbar() {
+  const navlinksCSS = useRef();
+  const bur1 = useRef();
+  const bur2 = useRef();
+  const bur3 = useRef();
+  const burgerClicked = () => {
+    navlinksCSS.current.classList.toggle("burger-clicked");
+    bur1.current.classList.toggle("skew1");
+    bur2.current.classList.toggle("skew2");
+    bur3.current.classList.toggle("display-none");
+  };
+
+  return (
+    <div>
+      <div className="navbar">
+        <div className="logo">
+          <img src={logo} alt="dipam-logo" />
+        </div>
+        <div className="navlinks" ref={navlinksCSS}>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/dipam-poudel/">
+            LINKEDIN
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://twitter.com/deepum__poudel">
+            TWITTER
+          </a>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.instagram.com/im.jack.skellington/"
+          >
+            INSTAGRAM
+          </a>
+          <a target="_blank" rel="noreferrer" href="https://github.com/Dipam1">
+            GITHUB
+          </a>
+        </div>
+        <div className="burger" onClick={burgerClicked}>
+          <div className="bur1" ref={bur1}></div>
+          <div className="bur2" ref={bur2}></div>
+          <div className="bur3" ref={bur3}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
