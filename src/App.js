4ffb0072c6dee5bb6fa064d439fb6fd787fe9d91
deepum <deepumpoudel@gmail.com>
2021-01-30 17:47:04 +0545

    checking netlify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from './Components/Navbar/Navbar';
 import FrontPageTopCard from './Components/FrontPageTopCard/FrontPageTopCard';
+import image from './Images/logo.png'
 
 
 function App() {
