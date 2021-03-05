3dbe7c3e7495da1e0eb5f5b23ded95494826b63c
deepum <deepumpoudel@gmail.com>
2021-03-05 20:51:14 +0545

    meeow

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,18 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Home.css";
 
-function Home() {
+const Home = () => {
+  const [textChange, settextChange] = useState("i like movies");
+  useEffect(() => {
+    setTimeout(() => {
+      if (textChange === "i like movies") {
+        settextChange("i like football");
+      } else if (textChange === "i like football") {
+        settextChange("i also kinda like me");
+      } else {
+        settextChange("but most importantly, i like you:)");
+      }
+    }, 2000);
+  }, [textChange]);
+
   return (
-    <div className="content">
+    <>
       <div className="home">
-        <h1>
-          Get off me site,
-          <br /> you lil shite
-          <br />
-        </h1>
+        <h1>{textChange}</h1>
       </div>
-    </div>
+      <h1 style={{ marginTop: "30vh" }}>
+        Unless you're ramesh, fuck you ramesh. i hate your ass.
+      </h1>
+    </>
   );
-}
+};
 
 export default Home;
